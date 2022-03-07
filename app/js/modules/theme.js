88ae2d0cae1f7cb590a03ea69fc70696a21d9009
polemico <polemico@pop-os.localdomain>
2022-03-07 16:13:04 -0300

    saving the chosen theme in local storage

--- app/js/modules/theme.js
+++ app/js/modules/theme.js
@@ -1,23 +1,26 @@
 const body = document.querySelector('body');
-const button = document.querySelector('#btn-theme');
-const text = button.querySelector('span');
-const icon = button.querySelector('i');
+const btnSwitchTheme = document.querySelector('#btn-theme');
 
-const changeTheme = () => {
-    body.classList.toggle('dark-theme');
-};
+if (btnSwitchTheme) {
+    const initTheme = () => {
+        const darkTheme = localStorage.getItem('theme');
 
-const changeIcon = () => {
-    if (body.classList.contains('dark-theme')) {
-        icon.className = 'bx bxs-sun';
-        text.innerText = 'Light Mode';
-    } else {
-        icon.className = 'bx bxs-moon';
-        text.innerText = 'Dark Mode';
-    }
-};
+        const darkThemeSelected =
+            darkTheme !== null && darkTheme === 'dark-theme';
 
-button.addEventListener('click', () => {
-    changeTheme();
-    changeIcon();
-});
+        if (darkThemeSelected === true) {
+            body.classList.add('dark-theme');
+        }
+    };
+
+    const resetTheme = () => {
+        body.classList.toggle('dark-theme');
+
+        body.classList.contains('dark-theme')
+            ? localStorage.setItem('theme', 'dark-theme')
+            : localStorage.removeItem('theme');
+    };
+
+    initTheme();
+    btnSwitchTheme.addEventListener('click', resetTheme);
+}
